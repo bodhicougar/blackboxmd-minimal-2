d3623fe4173f6215a66265897fbfa545c48ff2ec
bodhicougar <info@blackboxmd.fyi>
2020-01-28 15:20:09 +0100

    Images Optimierungsversuch

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,6 +40,23 @@ module.exports = {
       }
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 600
+            }
+          },
+          `gatsby-remark-lazy-load`
+        ]
+      }
+    },
+    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
